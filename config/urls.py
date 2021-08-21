e0257769f17868b4c7a9c36ee621b74e102a8585
mehriddinnozimov05 <mehriddinnozimov05@gmail.com>
2021-08-21 12:42:33 +0500

    v2

--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path("", views.home, name="home"),
-    path("store/", include("store.urls")
-)
+    path('admin/', admin.site.urls),
+    path("store/", include("store.urls")),
+    path("cart/", include("carts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,6 +1,13 @@
+from django.http.response import HttpResponse
+from carts.models import CartItem
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+
+
 
 def store(request, category_slug=None):
     categories = None
@@ -11,23 +18,44 @@ def store(request, category_slug=None):
         products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by("id")
         product_count = products.count()
     
+    paginator = Paginator(products, 2)
+    page = request.GET.get('page')
+    paged_product = paginator.get_page(page)
     context = {
-        'products': products,
+        'products': paged_product,
         'count': product_count
     }
 
     return render(request, 'store/store.html', context)
 
+
+
+
 def prod_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
     context = {
         'product': single_product,
+        'in_cart': in_cart
 
     }
     return render(request, "store/product_detail.html", context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET["keyword"]
+        if keyword:
+            products = Product.objects.order_by("-created_date").filter(Q(prod_description__icontains=keyword) | Q(prod_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        "products": products,
+        "count": product_count
+    }
+    return render(request, "store/store.html", context)
